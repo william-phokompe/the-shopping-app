dfa9265bc79298a9153714f31ed541d3ed71978f
William Phokompe <phokompe.william@outlook.com>
2021-05-18 21:57:38 +0200

    ✨ Add Side drawer and create Order's screen with reducer+action

---
+++ components/screens/shop/OrdersScreen.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { FlatList, Text, Plat } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+
+import HeaderButton from "../../UI/HeaderButton";
+
+
+const OrdersScreen = () => {
+  const orders = useSelector((state) => state.orders.orders);
+
+  return (
+    <FlatList
+      data={orders}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => <Text>{itemData.item.totalAmount}</Text>}
+    />
+  );
+};
+
+OrdersScreen.navigationOptions = navigationData => {
+  return{
+    headerTitle: 'Your Orders',
+    headerLeft: (_) => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navigationData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    )
+  }
+}
+
+export default OrdersScreen;

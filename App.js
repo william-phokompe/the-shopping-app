1b91526dc07bf970abc1d66355a5b43583ea7f30
William Phokompe <phokompe.william@outlook.com>
2021-05-18 03:24:09 +0200

    ✨ load fonts onto project

--- App.js
+++ App.js
@@ -1,17 +1,42 @@
-import React from "react";
-import {createStore, combineReducers} from "redux";
-import {Provider} from "react-redux";
+import React, { useState } from "react";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { enableScreens } from "react-native-screens";
 
-import productReducer from './store/reducers/products'
-import ShopNavigator from './navigation/ShopNavigator'
+import productReducer from "./store/reducers/products";
+import ShopNavigator from "./navigation/ShopNavigator";
+
+enableScreens();
 
 const rootReducer = combineReducers({
-  products: productReducer
-})
+  products: productReducer,
+});
 
 const store = createStore(rootReducer);
 
+const fetchFonts = (_) => {
+  return Font.loadAsync({
+    "nanum-myeongjo-bold": require("./assets/fonts/NanumMyeongjo-Bold.ttf"),
+    "nanum-myeongjo-extrabold": require("./assets/fonts/NanumMyeongjo-ExtraBold.ttf"),
+    "nanum-myeongjo-regular": require("./assets/fonts/NanumMyeongjo-Regular.ttf"),
+  });
+};
+
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={(_) => setFontLoaded(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />

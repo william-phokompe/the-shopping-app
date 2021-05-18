b3f465f85caf944777ac1b550773693deff08f09
William Phokompe <phokompe.william@outlook.com>
2021-05-18 02:49:54 +0200

    🚧 Touchable component on ProductItem

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,28 +1,50 @@
 import React from "react";
-import { View, Text, Image, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Platform,
+} from "react-native";
 import Colors from "../constants/Colors";
 
 const ProductItem = (props) => {
+  let TouchableComponent = TouchableOpacity;
+
+  if (Platform.OS === "android" && Platform.Version >= 21)
+    TouchableComponent = TouchableNativeFeedback;
   return (
     <View style={styles.product}>
-      <View style={styles.imageContainer}>
-        <Image style={styles.image} source={{ uri: props.prod.imageUrl }} />
-      </View>
-      <View style={styles.details}>
-        <Text style={styles.title}>{props.prod.title}</Text>
-        <Text style={styles.price}>${props.prod.price.toFixed(2)}</Text>
-      </View>
-      <View style={styles.actions}>
-        <Button
-          color={Colors.primary}
-          title="View Details"
-          onPress={props.onViewDetail}
-        />
-        <Button
-          color={Colors.primary}
-          title="Add To Cart"
-          onPress={props.onAddToCard}
-        />
+      <View style={styles.touchable}>
+        <TouchableComponent onPress={props.onViewDetail} useForeground>
+          <View>
+            <View style={styles.imageContainer}>
+              <Image
+                style={styles.image}
+                source={{ uri: props.prod.imageUrl }}
+              />
+            </View>
+            <View style={styles.details}>
+              <Text style={styles.title}>{props.prod.title}</Text>
+              <Text style={styles.price}>${props.prod.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+              <Button
+                color={Colors.primary}
+                title="View Details"
+                onPress={props.onViewDetail}
+              />
+              <Button
+                color={Colors.primary}
+                title="Add To Cart"
+                onPress={props.onAddToCard}
+              />
+            </View>
+          </View>
+        </TouchableComponent>
       </View>
     </View>
   );
@@ -39,6 +61,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     height: 300, // NB: possible dynamic resize using dimentions
     margin: 20,
+    overflow: "hidden",
   },
 
   imageContainer: {
@@ -76,6 +99,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     height: "15%",
     padding: 10,
+  },
+
+  touchable: {
+    borderRadius: 10,
+    overflow: "hidden",
   },
 });
 
